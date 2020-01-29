185e4f64ad8615e65bd986b5bc1d93440624b280
LatifiEmami <morteza.latifiemami@cbc.de>
2020-01-29 16:39:04 +0100

    Project Exercises for ARVATO Created!

---
+++ src/main/java/com/arvato/exercise/latifi/LatifiApplication.java
@@ -0,0 +1,24 @@
+package com.arvato.exercise.latifi;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LatifiApplication implements CommandLineRunner {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatifiApplication.class, args);
+
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/test/java/com/arvato/exercise/latifi/LatifiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvato.exercise.latifi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatifiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
